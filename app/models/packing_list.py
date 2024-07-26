3d43ade62a914625860ed130fe5c640c6c06881f
Ivana <ivana.a.maldonado@gmail.com>
2024-07-26 01:42:35 -0400

    Add Packing List model

---
+++ app/models/packing_list.py
@@ -0,0 +1,19 @@
+from app import db
+
+class PackingList(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    item_name = db.Column(db.String(100), nullable=False)
+    is_packed = db.Column(db.Boolean, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', back_populates='packing_list')
+
+# Converts PackingList instance into a dictionary:
+# Helps with serialization to send data as JSON in API responses.
+
+def to_dict(self):
+    return {
+        'id': self.id,
+        'item_name': self.item_name,
+        'is_packed': self.is_packed,
+        'user_id': self.user_id
+    }
