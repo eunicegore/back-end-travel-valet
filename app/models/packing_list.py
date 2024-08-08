c5f8bcdf0550cba2c1aef8bdfef4812ea368ebac
Ivana <ivana.a.maldonado@gmail.com>
2024-08-08 15:59:24 -0400

    Update models

--- app/models/packing_list.py
+++ app/models/packing_list.py
@@ -1,18 +1,20 @@
+from datetime import datetime
 from app import db
 
 class PackingList(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     list_name = db.Column(db.String(100), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    
+    date_saved = db.Column(db.DateTime, default=datetime.utcnow) 
     items = db.relationship('PackingListItem', backref='packing_list', lazy=True, cascade='all, delete-orphan')
 
 # Converts PackingList instance into a dictionary:
 # Helps with serialization to send data as JSON in API responses.
     def to_dict(self):
         return {
-            'list_id': self.id,
-            'list_name': self.list_name,
-            'user_id': self.user_id,
+            'id': self.id,
+            'listName': self.list_name,
+            'userId': self.user_id,
+            'dateSaved': self.date_saved.strftime('%Y-%m-%d'),
             'items': [item.to_dict() for item in self.items]
         }
--- app/models/packing_list_item.py
+++ app/models/packing_list_item.py
@@ -4,17 +4,15 @@ class PackingListItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     item_name = db.Column(db.String(100), nullable=False)
     quantity = db.Column(db.Integer, nullable=False, default=1)
-    packed_quantity = db.Column(db.Integer, nullable=False, default=0)
     is_packed = db.Column(db.Boolean, default=False)
-    
     packing_list_id = db.Column(db.Integer, db.ForeignKey('packing_list.id'), nullable=False)
 
 
     def to_dict(self):
         return {
-            'item_id': self.id,
-            'item_name': self.item_name,
+            'id': self.id,
+            'description': self.item_name,
             'quantity': self.quantity,
-            'packed_quantity': self.packed_quantity,
-            'is_packed': self.is_packed
+            'packed': self.is_packed,
+            'listId':self.packing_list_id   # Association with the parent packing list
         }
